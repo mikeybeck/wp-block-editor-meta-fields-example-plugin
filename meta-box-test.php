15708bcc084c6b5302123067f0d3e420ec81b17c
Mike Beck <mike@mikeybeck.com>
2024-07-23 14:00:03 +1200

    initial commit

---
+++ meta-box-test.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+Plugin Name: Sidebar example
+*/
+
+function sidebar_plugin_register() {
+    wp_register_script(
+        'plugin-sidebar-js',
+        plugins_url( 'plugin-sidebar.js', __FILE__ ),
+        array(
+            'react',
+            'wp-plugins',
+            'wp-editor',
+            'wp-components',
+            'wp-data',
+        )
+    );
+//    wp_register_style(
+//        'plugin-sidebar-css',
+//        plugins_url( 'plugin-sidebar.css', __FILE__ )
+//    );
+}
+add_action( 'init', 'sidebar_plugin_register' );
+
+function sidebar_plugin_add_meta_block_field() {
+    register_post_meta( 'post', 'sidebar_plugin_meta_block_field', array(
+        'show_in_rest' => true,
+        'single' => true,
+        'type' => 'string',
+    ) );
+    register_post_meta( 'page', 'sidebar_plugin_meta_block_field', array(
+        'show_in_rest' => true,
+        'single' => true,
+        'type' => 'string',
+    ) );
+}
+add_action( 'init', 'sidebar_plugin_add_meta_block_field' );
+
+
+function custom_post_type_support() {
+    add_post_type_support( 'post', 'custom-fields' );
+    add_post_type_support( 'page', 'custom-fields' );
+}
+add_action( 'init', 'custom_post_type_support' );
+
+function sidebar_plugin_script_enqueue() {
+    wp_enqueue_script( 'plugin-sidebar-js' );
+    wp_enqueue_style( 'plugin-sidebar-css' );
+}
+add_action( 'enqueue_block_editor_assets', 'sidebar_plugin_script_enqueue' );
+
+
